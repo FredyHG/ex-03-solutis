15ead123bfc48c8291c4d99d7c1bb5e4c958f931
Luiz Casais <lccjesus@gmail.com>
2024-08-17 20:56:05 -0300

    teste de commit

--- Conjunto_2/Atividade09.java
+++ Conjunto_2/Atividade09.java
@@ -1,7 +1,23 @@
 package Conjunto_2;
 
-public class Atividade09 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+    public class Atividade09 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite o seu ano de nascimento: ");
+            int anoNascimento = scanner.nextInt();
+
+            int anoAtual = 2023; // ano atual (você pode mudar isso para obter o ano atual dinamicamente)
+
+            int idade = anoAtual - anoNascimento;
+
+            if (idade >= 18) {
+                System.out.println("Você é maior de idade.");
+            } else {
+                System.out.println("Você é menor de idade.");
+            }
+        }
     }
-}
+
