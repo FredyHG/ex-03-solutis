8db8ca3637f4aa3cc211c104dd48478aed8efec2
fredyhg <fredyultima2@gmail.com>
2024-08-17 21:27:30 -0300

    resolved exercise 07

--- Conjunto_1/Atividade07.java
+++ Conjunto_1/Atividade07.java
@@ -2,6 +2,10 @@ package Conjunto_1;
 
 public class Atividade07 {
     public static void main(String[] args) {
+        double billAmount = 150.0;
+        double commissionRate = 0.10;
+        double commission = billAmount * commissionRate;
 
+        System.out.println("The waiter's 10% commission is: $" + commission);
     }
 }
