299b79553b749f2b773d8c5ebb561bef382851dd
fredyhg <fredyultima2@gmail.com>
2024-08-17 22:31:09 -0300

    resolved exercise 57

--- Conjunto_3/Atividade57.java
+++ Conjunto_3/Atividade57.java
@@ -12,24 +12,22 @@ public class Atividade57 {
         return LocalDate.parse(dateString, formatter);
     }
 
+    private static int readNumberOfDays(Scanner scanner) {
+        System.out.print("Enter the number of days to add: ");
+        return scanner.nextInt();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         LocalDate startDate = readDate(scanner, "Enter the start date (dd/MM/yyyy): ");
-        LocalDate endDate = readDate(scanner, "Enter the end date (dd/MM/yyyy): ");
 
-        if (startDate.isAfter(endDate)) {
-            System.out.println("Start date must be before or equal to end date.");
-            return;
-        }
+        int numberOfDays = readNumberOfDays(scanner);
 
-        System.out.println("Dates between " + startDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + " and " + endDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ":");
-        LocalDate currentDate = startDate;
+        LocalDate endDate = startDate.plusDays(numberOfDays);
 
-        while (!currentDate.isAfter(endDate)) {
-            System.out.println(currentDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-            currentDate = currentDate.plusDays(1);
-        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println(startDate.format(formatter) + " + " + numberOfDays + " = " + endDate.format(formatter));
 
         scanner.close();
     }
