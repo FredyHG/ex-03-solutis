e0e5fe92c74015d9fc6233701afa2b2a0470ee55
renatof4rias <renatof4rias@gmail.com>
2024-08-19 22:52:44 -0300

    feat: Conjunto 3 - Atividade 40

--- Conjunto_3/Atividade40.java
+++ Conjunto_3/Atividade40.java
@@ -1,7 +1,33 @@
 package Conjunto_3;
 
+import java.util.Scanner;
 public class Atividade40 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite a quantidade de pessoas: ");
+        int quantidade = sc.nextInt();
+
+        int[] idades = new int[quantidade];
+        int somaIdades = 0;
+        int maiorIdade = Integer.MIN_VALUE;
+        int menorIdade = Integer.MAX_VALUE;
+        int maiorDeIdade = 0;
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print("Digite a idade da pessoa " + (i + 1) + ": ");
+            idades[i] = sc.nextInt();
+            somaIdades += idades[i];
+            if (idades[i] > maiorIdade) maiorIdade = idades[i];
+            if (idades[i] < menorIdade) menorIdade = idades[i];
+            if (idades[i] >= 18) maiorDeIdade++;
+        }
+
+        double mediaIdades = (double) somaIdades / quantidade;
+
+        System.out.println("Média das idades: " + mediaIdades);
+        System.out.println("Maior idade: " + maiorIdade);
+        System.out.println("Menor idade: " + menorIdade);
+        System.out.println("Quantidade de pessoas maiores de idade: " + maiorDeIdade);
     }
 }
