4336fdcba7bde60081b906a47038f226a92b965f
renatof4rias <renatof4rias@gmail.com>
2024-08-19 22:50:46 -0300

    feat: Conjunto 3 - Atividade 37

--- Conjunto_3/Atividade37.java
+++ Conjunto_3/Atividade37.java
@@ -1,7 +1,18 @@
 package Conjunto_3;
 
+import java.util.Scanner;
 public class Atividade37 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número para calcular o fatorial: ");
+        int num = sc.nextInt();
+
+        long fatorial = 1;
+        for (int i = 2; i <= num; i++) {
+            fatorial *= i;
+        }
+        System.out.println("O fatorial de " + num + " é: " + fatorial);
 
     }
 }
